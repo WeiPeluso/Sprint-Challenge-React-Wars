4cd3dbbb0391b0e414351de91be38b634c5b4632
wei peluso <weipeluso@gmail.com>
2020-05-15 12:50:45 -0400

    finished search by name, try to add by episoda

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import './App.css';
+import React, {useState,useEffect} from 'react';
+import './App.scss';
+import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,78 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters]=useState([])
+  const [searchName,setSearchName]=useState([])
+  const [page,setPage]=useState(1)
+  const [information,setInformation]=useState([])
+
+  //useEffect(()=>{console.log(characters)})
+
+  useEffect(()=>{
+
+    axios.get(`https://rickandmortyapi.com/api/character/?name=${searchName}&page=${page}&name=${searchName}`)
+
+    .then((response)=>{
+
+      setCharacters(response.data.results)
+      setInformation(response.data.info)
+  
+    })
+    .catch((error)=>{
+      setPage(1)
+      console.log(error)
+    })
+  },[searchName,page])
+
+
+  const prevHandler=(event)=>{
+    event.preventDefault()
+    if(page>1){
+      setPage(page -1)
+    }else{
+      setPage(information.pages)
+    }
+  }
+
+  
+  const nextHandler=(event)=>{
+    event.preventDefault()
+    if(page<=information.pages){
+      setPage(page+1)
+    }else{
+      setPage(1)
+    }
+  }
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+
+      <div>
+          <h1>Welcome to the Rick and Morty's Family</h1>
+      </div>
+ 
+      <div className='searchArea'>
+            <p>Search By Name</p>
+            <input type='text' onChange={(event)=>{
+          setSearchName(event.target.value)
+        }} 
+        value={searchName}></input>
+
+        </div>
+
+        <div className="pageArea">
+        <button onClick={prevHandler}>pre</button>
+        <p>{page}/{information.pages}</p>
+        <button onClick={nextHandler}>next</button>
+
+        </div>
+      
+      <div className="displayCharacter">
+      {characters.map((character)=> (
+            <Character key={character.id} character={character} />
+            ))
+      }
+     </div>
     </div>
   );
 }
